07326c983956894e0ce43175060fb00012d532e4
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2022-11-11 12:31:31 +0600

    created navbar with website name

---
+++ src/pages/Shared/Footer/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo from '../../../../src/logo.svg'
+import { CDBFooter, CDBBox, CDBBtn, CDBIcon } from 'cdbreact';
+
+
+const Footer = () => {
+    return (
+        <CDBFooter className="shadow">
+            <CDBBox
+                display="flex"
+                justifyContent="between"
+                alignItems="center"
+                className="mx-auto py-4 flex-wrap"
+                style={{ width: '80%' }}
+            >
+                <CDBBox display="flex" alignItems="center">
+                    <a href="/" className="d-flex align-items-center p-0 text-dark">
+                        <img
+                            alt="logo"
+                            src={logo}
+                            width="100px"
+                        />
+                        <span className="ml-4 h5 mb-0 font-weight-bold">eFitTrainer</span>
+                    </a>
+                </CDBBox>
+                <CDBBox>
+                    <small className="ml-2">&copy; Devwares, 2022. All rights reserved.</small>
+                </CDBBox>
+                <CDBBox display="flex">
+                    <CDBBtn flat color="dark" className="p-2">
+                        <CDBIcon fab icon="facebook-f" />
+                    </CDBBtn>
+                    <CDBBtn flat color="dark" className="mx-3 p-2">
+                        <CDBIcon fab icon="twitter" />
+                    </CDBBtn>
+                    <CDBBtn flat color="dark" className="p-2">
+                        <CDBIcon fab icon="instagram" />
+                    </CDBBtn>
+                </CDBBox>
+            </CDBBox>
+        </CDBFooter>
+    );
+};
+
+export default Footer;
